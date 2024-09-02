ad4825b532378b4f132583eabb0c13b41b8a94e3
Fireswarm <tomekk@shaw.ca>
2024-09-02 15:33:03 -0600

    Started working on file reading

--- bedrockReader.py
+++ bedrockReader.py
@@ -1,3 +1,35 @@
+def convertNameBE(namePath):
+    #needs to read name from file
+    nameFile = open(namePath, "r")
+    readName = nameFile.read()
+    nameFile.close()
+    return readName
+
+def translateLevelDataBE(levelPath, oldLevelPath):
+    #needs to read and interpret data from level files
+    levelData = []
+
+    #first read in regular levelData
+    levelFile = open(levelPath, "rb")
+    levelData.append(levelFile.read())
+    levelFile.close()
+
+    #now the old levelData
+    levelFile = open(oldLevelPath, "rb")
+    levelData.append(levelFile.read())
+    levelFile.close()
+    
+    return levelData
+
+def parseDB(dbFolder):
+    readDatabase = "A bunch of junk that we unpack"
+    return readDatabase
+
 def readBEFile(filepath):
-    pass
+    #Conver World Metadata
+    convertNameBE(filepath+"/levelname.txt")
+    translateLevelDataBE(filepath+"/level.dat", filepath+"level.dat_old")
+
+    #Convert DataBase
+    parseDB(filepath+"/db")
 
